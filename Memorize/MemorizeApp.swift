27ca6a84373d6e4258c5ffb6329a1a227ac86720
YuraRov <yura.rov555@gmail.com>
2023-02-11 20:49:23 +0200

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Admin on 2023-02-11.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
