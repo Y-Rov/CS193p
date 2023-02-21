6d3eaa4634e7a83a0fbeb420dfca36dc1b2418bb
YuraRov <yura.rov555@gmail.com>
2023-02-21 10:36:53 +0200

    Removed code from the first assignment

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -10,47 +10,7 @@ import SwiftUI
 struct ContentView: View {
     @ObservedObject var game: EmojiMemoryGame
     
-//    var vehicles = ["🚄", "🚃", "🚠", "🚌", "🚎", "🚗", "🏎", "🏍", "🚑", "🚲", "🚂", "🛸", "🚁"]
-//    var buildings = ["🏠", "🏨", "💒", "🏣", "🏭", "🏪", "🏦", "🕌", "🏩", "🏛", "🏬", "🕍", "🏥", "⛩"]
-//    var food = ["🍏", "🍐", "🍊", "🍋", "🍌", "🍉", "🍓", "🍒", "🥥", "🍑", "🍆", "🥑", "🥦", "🥕", "🥐"]
-//    var animals = ["🐍", "🦎", "🦖", "🐙", "🐳", "🐬", "🐊", "🐝", "🐴", "🐺", "🦇", "🦋", "🦆", "🦄", "🐉", "🐈"]
-//
-//    @State var emojis = ["🚄", "🚃", "🚠", "🚌", "🚎", "🚗", "🏎", "🏍", "🚑", "🚲", "🚂", "🛸", "🚁"]
-//
-//    @State var emojiCount = 4;
-//    let minEmojiCount = 4;
-    
     var body: some View {
-//        VStack {
-//            Text("Memorize!")
-//                .font(.largeTitle)
-//                .padding(.vertical)
-//            ScrollView {
-//                LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
-//                    ForEach(game.cards) { card in
-//                        CardView(card: card)
-//                            .aspectRatio(2/3, contentMode: .fit)
-//                            .onTapGesture {
-//                                game.choose(card)
-//                            }
-//                    }
-//                }
-//            }
-//            .foregroundColor(.red)
-//            Spacer()
-//            HStack {
-//                Spacer()
-//                vehicleThemeButton
-//                Spacer()
-//                buildingThemeButton
-//                Spacer()
-//                foodThemeButton
-//                Spacer()
-//                animalThemeButton
-//                Spacer()
-//            }
-//        }
-//        .padding(.horizontal)
         ScrollView {
             LazyVGrid(columns: [GridItem(.adaptive(minimum: 75))], spacing: 10) {
                 ForEach(game.cards) { card in
@@ -65,52 +25,6 @@ struct ContentView: View {
         .foregroundColor(.red)
         .padding(.horizontal)
     }
-    
-//    var vehicleThemeButton: some View {
-//        Button(action: {
-//            emojis = vehicles.shuffled()
-//            emojiCount = Int.random(in: minEmojiCount..<vehicles.count)
-//        }, label: {
-//            createButtonLabel(imageName: "car", caption: "Vehicles")
-//        })
-//    }
-//
-//    var buildingThemeButton: some View {
-//        Button(action: {
-//            emojis = buildings.shuffled()
-//            emojiCount = Int.random(in: minEmojiCount..<buildings.count)
-//        }, label: {
-//            createButtonLabel(imageName: "house", caption: "Buildings")
-//        })
-//    }
-//
-//    var foodThemeButton: some View {
-//        Button(action: {
-//            emojis = food.shuffled()
-//            emojiCount = Int.random(in: minEmojiCount..<food.count)
-//        }, label: {
-//            createButtonLabel(imageName: "fork.knife", caption: "Food")
-//        })
-//    }
-//
-//    var animalThemeButton: some View {
-//        Button(action: {
-//            emojis = animals.shuffled()
-//            emojiCount = Int.random(in: minEmojiCount..<animals.count)
-//        }, label: {
-//            createButtonLabel(imageName: "pawprint", caption: "Animals")
-//        })
-//    }
-//
-//    func createButtonLabel(imageName: String, caption: String) -> some View {
-//        VStack {
-//            Image(systemName: imageName)
-//                .font(.largeTitle)
-//            Text(caption)
-//                .font(.caption)
-//        }
-//        .padding(.horizontal)
-//    }
 }
 
 struct CardView: View {
