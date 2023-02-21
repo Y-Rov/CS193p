1eece7c0d7e769156c2110e820bd86d359852ecc
YuraRov <yura.rov555@gmail.com>
2023-02-21 11:07:03 +0200

    Added the formal concept of a “GameTheme”

---
+++ Memorize/GameTheme.swift
@@ -0,0 +1,15 @@
+//
+//  GameTheme.swift
+//  Memorize
+//
+//  Created by user on 2/21/23.
+//
+
+import Foundation
+
+struct GameTheme {
+    var name: String
+    var emojis: Set<String>
+    var numberOfPairsOfCards: Int
+    var color: String
+}
